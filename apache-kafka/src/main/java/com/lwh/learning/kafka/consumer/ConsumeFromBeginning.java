2c94a6118ee376577f08623ecbe1b31fe88f249a
yukiofyume <853036144@qq.com>
2024-08-15 00:02:33 +0800

    first commit

---
+++ apache-kafka/src/main/java/com/lwh/learning/kafka/consumer/ConsumeFromBeginning.java
@@ -0,0 +1,37 @@
+package com.lwh.learning.kafka.consumer;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * @author lwh
+ * @date 2024-08-14 23:43:32
+ * @describe -
+ */
+public class ConsumeFromBeginning {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(ConsumeFromBeginning.class);
+
+    private static void setUp() {
+        Properties consumerProperties = new Properties();
+        consumerProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        consumerProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        consumerProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        consumerProperties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        consumerProperties.put(ConsumerConfig.GROUP_ID_CONFIG, "MyFirstConsumer");
+        try (Consumer<Long, String> consumer = new KafkaConsumer<>(consumerProperties)) {
+            String topic = "my-first-topic";
+            consumer.subscribe(List.of(topic));
+            consumer.poll(Duration.ofMinutes(1))
+                    .forEach(System.out::println);
+        }
+    }
+}
